84238a8dad21291b863fa4b49da27d5cb582a334
victor <you@example.com>
2019-02-13 08:02:30 +0100

    Opciones varias

--- src/app/Componente/reporte/reporte.component.ts
+++ src/app/Componente/reporte/reporte.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 import { ServicesService, Reporte } from '../../service/services.service';
 
 @Component({
@@ -19,5 +20,22 @@ export class ReporteComponent implements OnInit {
   ngOnInit() {
     this.reportes = this.reportesService.getReportes();
   }
+  convert() {
+    var doc = new jsPDF();
+    this.reportes.forEach(function(employee, i){
+        doc.text(5, 10 + (i * 20), 
+            "Titulo: " + employee.titulo +
+             " / "+
+            "Fecha: " + employee.fecha);
+        });
+        this.reportes.forEach(function(employee, i){
+          doc.text(10, 20 + (i * 20), 
+              "Funcion: " + employee.funcion +
+              "Comentario: " + employee.comentario);
+        });
+      
+    doc.save('Test.pdf');
+  }
+  
 
 }
